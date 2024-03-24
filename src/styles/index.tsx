68e1e7466c37baf7926f3b70081388eff9b5d1e6
Miguel Gutierrez <gt2rz.dev@gmail.com>
2024-03-24 07:27:25 -0500

    Add screenStyles module for styling screens

---
+++ src/styles/index.tsx
@@ -0,0 +1,1 @@
+export {screenStyles} from './screenStyles';
---
+++ src/styles/screenStyles.tsx
@@ -0,0 +1,11 @@
+export const screenStyles = {
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+};
