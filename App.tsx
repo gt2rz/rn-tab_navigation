2c9f5799ad1104e30381bd3c66a8554e9aff1db6
Miguel Gutierrez <gt2rz.dev@gmail.com>
2024-03-25 17:31:05 -0500

    Refactor App.tsx to use bottom tab navigation and update dependencies

--- App.tsx
+++ App.tsx
@@ -1,17 +1,38 @@
 import React from 'react';
-import {Text, View} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 
 import HomeScreen from './src/screens/HomeScreen';
 import SettingsScreen from './src/screens/SettingsScreen';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 
+import Ionicons from 'react-native-vector-icons/Ionicons';
+
 const Tab = createBottomTabNavigator();
 
 function App(): React.JSX.Element {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
+      <Tab.Navigator
+        screenOptions={({route}) => ({
+          // eslint-disable-next-line react/no-unstable-nested-components
+          tabBarIcon: ({focused, color, size}) => {
+            let iconName;
+
+            if (route.name === 'Home') {
+              iconName = focused
+                ? 'information-circle'
+                : 'information-circle-outline';
+            } else if (route.name === 'Settings') {
+              iconName = focused ? 'list' : 'list-outline';
+            }
+
+            iconName = iconName || 'information-circle-outline';
+            // You can return any component that you like here!
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+          tabBarActiveTintColor: 'tomato',
+          tabBarInactiveTintColor: 'gray',
+        })}>
         <Tab.Screen name="Home" component={HomeScreen} />
         <Tab.Screen name="Settings" component={SettingsScreen} />
       </Tab.Navigator>
