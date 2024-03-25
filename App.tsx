54321c147dfa7de68ec08402cfd8ce7a40f15521
Miguel Gutierrez <gt2rz.dev@gmail.com>
2024-03-25 17:33:04 -0500

    Update tabBar options in App.tsx to include tabBarBadge for HomeScreen

--- App.tsx
+++ App.tsx
@@ -33,7 +33,11 @@ function App(): React.JSX.Element {
           tabBarActiveTintColor: 'tomato',
           tabBarInactiveTintColor: 'gray',
         })}>
-        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{tabBarBadge: 3}}
+        />
         <Tab.Screen name="Settings" component={SettingsScreen} />
       </Tab.Navigator>
     </NavigationContainer>
